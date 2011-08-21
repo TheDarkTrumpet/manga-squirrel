f0b9b7ea7f29a95ceb13d44bc483be49730b03d8
Ali Lown <ali@lown.me.uk>
2011-08-21 20:15:38 +0100

    Remove rename, queue methods. Remove volume, chapter options

--- lib/manga-squirrel/cli.rb
+++ lib/manga-squirrel/cli.rb
@@ -34,56 +34,22 @@ module Manga
       desc 'cbz series [--out=dir]', 'Builds CBZs for all chapters for the specified series name'
       method_option :out, :default => "."
       def cbz(series)
-        self.makequeue QueueAction::Archive, {:series=>series.strip, :options=>{:out=>File.expand_path(options[:out])}}
+        Manga::Squirrel::Queuer.queue QueueAction::Archive, {:series=>series.strip, :options=>{:out=>File.expand_path(options[:out])}}
       end
 
-      desc 'queue series [--site=class --volumes=filter --chapters=filter]', 'Tries to fetch all chapters for given manga, skipping existing'
-      method_option :site, :default => "MangaFox"
-      method_option :volumes, :default => "true"
-      method_option :chapters, :default => "true"
-      def queue(series)
-        site = ("Manga::Squirrel::"+options[:site]).to_class
-        self.makequeue QueueAction::Download, {:site=>site,:series=>series,:options=>options}
-      end
-
-      desc 'fetch [--file=name --volumes=filter --chapters=filter]', 'Tries to fetch all mangas listed in filename, skipping any chapters already existing'
+      desc 'fetch [--file=name]', 'Tries to fetch all series listed in filename, skipping any chapters already existing'
       method_option :file, :default => "~/.ms"
-      method_option :volumes, :default => "true"
-      method_option :chapters, :default => "true"
       def fetch
         Manga::Squirrel::ConfigFile.parse(options[:file]).each do
           |name, site, raw, out, autocbz|
           puts "Fetching #{name]} from #{site}"
           begin
-            self.makequeue QueueAction::Download, [:site=>site, :series=>name, :options=>options]
+            Manga::Squirrel::Queuer.queue QueueAction::Download, [:site=>site, :series=>name, :options=>{:raw=>raw, :out=>out, :autocbz=>autocbz}]
           rescue
             #  puts "ERROR: Failed to fetch #{name}\n#{$0} #{$.}: #{$!}"
           end
         end
         f.close
-      end
-
-      desc 'rename series [--perform=true]', '**For upgrading between MS versions. Sanitizes all chapter names'
-      method_option :perform, :default => "false"
-      def rename(series)
-        Dir.glob(File.join(series,"*")).each {
-          |chapter|
-          if File.directory?(chapter) then
-            new_name = chapter.sanitize
-            unless new_name == chapter then
-              puts "Renamed #{chapter} to #{new_name}"
-              if eval(options[:perform]) then
-                File.rename(chapter,new_name)
-              end 
-            end
-          end
-        }
-      end
-
-      no_tasks do
-        def makequeue(action, options)
-          Manga::Squirrel::Queuer.queue action, options
-        end
       end
     end
   end
