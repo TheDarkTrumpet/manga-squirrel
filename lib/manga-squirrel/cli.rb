07e6db5f931bf91f4974d8fea5f22fdc00a70df5
Ali Lown <ali@lown.me.uk>
2011-08-21 20:13:18 +0100

    Add YAML parser

--- lib/manga-squirrel/cli.rb
+++ lib/manga-squirrel/cli.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'thor'
 require 'resque'
+require 'yaml'
 require 'manga-squirrel/common'
 require 'manga-squirrel/queuer'
 
@@ -38,36 +39,27 @@ module Manga
 
       desc 'queue series [--site=class --volumes=filter --chapters=filter]', 'Tries to fetch all chapters for given manga, skipping existing'
       method_option :site, :default => "MangaFox"
-	  method_option :volumes, :default => "true"
-	  method_option :chapters, :default => "true"
+      method_option :volumes, :default => "true"
+      method_option :chapters, :default => "true"
       def queue(series)
         site = ("Manga::Squirrel::"+options[:site]).to_class
         self.makequeue QueueAction::Download, {:site=>site,:series=>series,:options=>options}
       end
 
       desc 'fetch [--file=name --volumes=filter --chapters=filter]', 'Tries to fetch all mangas listed in filename, skipping any chapters already existing'
-      method_option :file, :default => ".ms"
-	  method_option :volumes, :default => "true"
-	  method_option :chapters, :default => "true"
+      method_option :file, :default => "~/.ms"
+      method_option :volumes, :default => "true"
+      method_option :chapters, :default => "true"
       def fetch
-        begin
-          f = File.open(options[:file], 'r')
-        rescue
-          puts "ERROR: File #{options[:file]} not found"
-          return
-        end
-        f.readlines.each {
-          |line|
-		  splits = line.strip.split('	')
-		  name = splits[0]
-		  site = ("Manga::Squirrel::"+splits[1]).to_class
-          puts "Fetching #{name} at #{site.inspect}"
+        Manga::Squirrel::ConfigFile.parse(options[:file]).each do
+          |name, site, raw, out, autocbz|
+          puts "Fetching #{name]} from #{site}"
           begin
-            self.makequeue QueueAction::Download, {:site=>site,:series=>name,:options=>options}
-          #rescue
-          #  puts "ERROR: Failed to fetch #{name}\n#{$0} #{$.}: #{$!}"
+            self.makequeue QueueAction::Download, [:site=>site, :series=>name, :options=>options]
+          rescue
+            #  puts "ERROR: Failed to fetch #{name}\n#{$0} #{$.}: #{$!}"
           end
-        }
+        end
         f.close
       end
 
@@ -78,19 +70,19 @@ module Manga
           |chapter|
           if File.directory?(chapter) then
             new_name = chapter.sanitize
-			unless new_name == chapter then
+            unless new_name == chapter then
               puts "Renamed #{chapter} to #{new_name}"
               if eval(options[:perform]) then
                 File.rename(chapter,new_name)
               end 
-			end
+            end
           end
         }
       end
 
       no_tasks do
         def makequeue(action, options)
-            Manga::Squirrel::Queuer.queue action, options
+          Manga::Squirrel::Queuer.queue action, options
         end
       end
     end
